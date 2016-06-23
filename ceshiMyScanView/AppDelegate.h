04e04a8c69516333bc27bd7288fd8d02b26f63a0
helong <hl@zehuaweiyedeimac.local>
2016-06-23 16:42:45 +0800

    Initial Commit

---
+++ ceshiMyScanView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ceshiMyScanView
+//
+//  Created by helong on 16/6/23.
+//  Copyright © 2016年 helong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ceshiMyScanView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ceshiMyScanView
+//
+//  Created by helong on 16/6/23.
+//  Copyright © 2016年 helong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
